5929ef761d9a6d4e2646003f4ebd2e63c2adea2a
Jonathan Hager <mail@hager-jonathan.de>
2024-01-26 17:06:46 +0100

    warning behoben

--- src/tt_um_alu.sv
+++ src/tt_um_alu.sv
@@ -30,7 +30,7 @@ module tt_um_alu // "tb.dut_inst"
    reg [3:0]         z;
 
    assign uo_out[3:0] = z;
-   assign uo_out[7:4] = 7'b0000;
+   assign uo_out[7:4] = 4'b0000;
    assign uio_oe[7:0] = 8'b00000000;
    assign uio_out[7:0] = 8'b00000000;
 
